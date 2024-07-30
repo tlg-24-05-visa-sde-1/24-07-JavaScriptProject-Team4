e7326473434e3570ab9f510e3029e3011b4f64a3
Summer Davis <summerd@tlgcohort.com>
2024-07-30 15:52:28 -0500

    first version fetch functioning

--- The-Common-Room/src/Components/FriendCard/FriendCard.jsx
+++ The-Common-Room/src/Components/FriendCard/FriendCard.jsx
@@ -5,7 +5,8 @@ export default function FriendCard({ name, house, image, handleAddNewFriend }) {
     <div className="card">
       <img className="card-img-top" src={image} alt={name} />
       <div className="card-body">
-        <h5 className="card-title">{house}</h5>
+        <h5 className="card-name">{name}</h5>
+        <h6 className="card-title">{house}</h6>
 
         <button onClick={handleAddNewFriend} className="btn btn-primary">
           Add New Friend
