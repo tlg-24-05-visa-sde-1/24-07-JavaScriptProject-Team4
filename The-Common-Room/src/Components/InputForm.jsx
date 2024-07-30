25da151c6e4ed288f14c782c5f9715603c1aa5ed
Tomiwa Akinbayo <tomiwaa@tlgcohort.com>
2024-07-29 19:11:27 -0500

    WP-inputForm

---
+++ The-Common-Room/src/Components/InputForm.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+function InputForm() {
+  return (
+    <>
+      <div className="form-floating mb-3">
+        <input
+          type="text"
+          className="form-control"
+          id="floatingInput"
+          placeholder="Name"
+        />
+        <label htmlFor="floatingInput">Name</label>
+      </div>
+      <div className="form-floating">
+        <input
+          type="text"
+          className="form-control"
+          id="floatingPassword"
+          placeholder="Snacky Snack"
+        />
+        <label htmlFor="floatingPassword">Favorite Snack</label>
+      </div>
+
+      <div className="input-group mb-3">
+        <label className="form-control" htmlFor="inputGroupSelect01">
+          What is most important to you?
+        </label>
+        <select className="form-select" id="inputGroupSelect01">
+          <option defaultValue>Choose...</option>
+          <option value="1">Love</option>
+          <option value="2">Wealth</option>
+          <option value="3">Peace</option>
+        </select>
+      </div>
+      <input className="btn btn-primary" type="submit" value="Submit"></input>
+    </>
+  );
+}
+
+export default InputForm;
---
+++ The-Common-Room/src/Pages/WelcomePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import InputForm from "../Components/InputForm";
+
+
+function WelcomePage() {
+  return (
+    <InputForm />
+
+  )
+}
+
+export default WelcomePage;
