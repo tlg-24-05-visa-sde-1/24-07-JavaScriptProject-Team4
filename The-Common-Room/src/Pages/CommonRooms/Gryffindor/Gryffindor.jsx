ad456fdfd7c2b41ce99fe1707dacaaf37a6c53c8
Tomiwa Akinbayo <tomiwaa@tlgcohort.com>
2024-07-31 19:50:06 -0500

    carousel completed for each common room

--- The-Common-Room/src/Pages/CommonRooms/Gryffindor/Gryffindor.jsx
+++ The-Common-Room/src/Pages/CommonRooms/Gryffindor/Gryffindor.jsx
@@ -1,12 +1,17 @@
 import Header from "../../../Components/Header/Header";
 import CommonRoom from "../../../Components/CommonRoom/CommonRoom";
 import "./Gryffindor.css";
+import { Carousel } from "../../../Components/Carousel/Carousel";
+import CrCarouselData from "../../../Components/CommonRoom/CommonRoomCarouselData";
 
 function Gryffindor() {
+  let data = CrCarouselData({ house: "gryffindor" });
+  // console.log(data);
   return (
     <div>
       <Header name="Gryffindor" />
       <CommonRoom />
+      <Carousel data={data} />
     </div>
   );
 }
--- The-Common-Room/src/Pages/CommonRooms/Ravenclaw/Ravenclaw.jsx
+++ The-Common-Room/src/Pages/CommonRooms/Ravenclaw/Ravenclaw.jsx
@@ -1,12 +1,16 @@
 import Header from "../../../Components/Header/Header";
 import CommonRoom from "../../../Components/CommonRoom/CommonRoom";
 import "./Ravenclaw.css";
+import { Carousel } from "../../../Components/Carousel/Carousel";
+import CrCarouselData from "../../../Components/CommonRoom/CommonRoomCarouselData";
 
 function Ravenclaw() {
+  let data = CrCarouselData({house: "ravenclaw"})
   return (
     <div>
       <Header name="Ravenclaw" />
       <CommonRoom />
+      <Carousel data={data} />
     </div>
   );
 }
